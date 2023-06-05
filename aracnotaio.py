be270a4052bf362b76bf7a0b0c40ce171e924044
DanieleLiprandi <daniele.liprandi@gmail.com>
2023-06-05 14:19:19 +0200

    aggiunta bozza di aggiunta spesa

--- aracnotaio.py
+++ aracnotaio.py
@@ -22,120 +22,210 @@ renamingdict = {
 
 negativecolumns = ["UsciteCassa", "UsciteBancaProssima", "UscitePaypal"]
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    
-    # ------------------------------ entrate uscite ------------------------------ #
-    dataframe = pd.read_excel(
-        uploaded_file, skiprows=[0, 1, 2, 3, 4, 5, 6, 8], header=0, index_col=1
-    ).rename(columns=renamingdict)
-    print(dataframe.head)
-    dataframe.loc[:,"DataOperazione"] = dataframe.loc[:,"DataOperazione"].apply(lambda x: x.replace(year = 2021))
-    dataframe.loc[:,"DataRegistrazione"] = dataframe.loc[:,"DataRegistrazione"].apply(lambda x: x.replace(year = 2021))
-    dataframe.loc[:,negativecolumns] = -dataframe.loc[:,negativecolumns]
-    dataframe.drop("Unnamed: 0", axis=1, inplace=True)
-    # dataframe = dataframe.loc[:,1:]
-    # dataframe = dataframe.dropna(axis=1, how='all')
-    st.write(dataframe)
+tabview, tabadd = st.tabs(["Analisi", "Aggiungi spesa"])
 
-    st.write("### Grafico nel tempo")
+with tabview:
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        # ------------------------------ entrate uscite ------------------------------ #
+        dataframe = pd.read_excel(
+            uploaded_file, skiprows=[0, 1, 2, 3, 4, 5, 6, 8], header=0, index_col=1
+        ).rename(columns=renamingdict)
+        print(dataframe.head)
+        dataframe.loc[:, "DataOperazione"] = dataframe.loc[:, "DataOperazione"].apply(
+            lambda x: x.replace(year=2021)
+        )
+        dataframe.loc[:, "DataRegistrazione"] = dataframe.loc[
+            :, "DataRegistrazione"
+        ].apply(lambda x: x.replace(year=2021))
+        dataframe.loc[:, negativecolumns] = -dataframe.loc[:, negativecolumns]
+        dataframe.drop("Unnamed: 0", axis=1, inplace=True)
+        # dataframe = dataframe.loc[:,1:]
+        # dataframe = dataframe.dropna(axis=1, how='all')
+        st.write(dataframe)
 
-    totalscatter = px.line(dataframe, x="DataOperazione", y="Totale")
-    st.plotly_chart(totalscatter)
+        st.write("### Grafico nel tempo")
 
-    # -------------------------------- description ------------------------------- #
-    st.write("## Analisi operazioni")
+        totalscatter = px.line(dataframe, x="DataOperazione", y="Totale")
+        st.plotly_chart(totalscatter)
 
-    analysecolumns = [
-                "EntrateCassa",
-                "UsciteCassa",
-                "EntrateBancaProssima",
-                "UsciteBancaProssima",
-                "EntratePaypal",
-                "UscitePaypal",
+        # -------------------------------- description ------------------------------- #
+        st.write("## Analisi operazioni")
+
+        analysecolumns = [
+            "EntrateCassa",
+            "UsciteCassa",
+            "EntrateBancaProssima",
+            "UsciteBancaProssima",
+            "EntratePaypal",
+            "UscitePaypal",
+        ]
+
+        df_sum = (
+            dataframe.loc[:, analysecolumns + ["Descrizione operazioni"]]
+            .groupby("Descrizione operazioni")
+            .sum()
+        )
+
+        df_melted = pd.melt(
+            df_sum.reset_index(),
+            id_vars="Descrizione operazioni",
+            value_vars=df_sum.reset_index().columns[1:],
+            var_name="Column",
+            value_name="Sum",
+        )
+
+        st.write("### Ammontare per operazione")
+
+        barplotadjacent = px.bar(
+            df_melted, x="Descrizione operazioni", y="Sum", color="Column"
+        )
+        st.plotly_chart(barplotadjacent)
+
+        st.write("### Entrate e uscite")
+        barplotstack = px.bar(
+            df_melted,
+            x="Column",
+            y="Sum",
+            color="Descrizione operazioni",
+            barmode="stack",
+        )
+        st.plotly_chart(barplotstack)
+
+        st.write("### Ammontare per singole operazioni")
+        st.write(df_sum.transpose())
+
+        # ---------------------------------------------------------------------------- #
+        #                                   Entrate                                    #
+        # ---------------------------------------------------------------------------- #
+
+        st.write("## Entrate per centri di spesa")
+        categoriesdf = dataframe.loc[:, "Quote associative":"Altro"]
+        categoriesdf["Descrizione operazioni"] = dataframe["Descrizione operazioni"]
+
+        df_sum_catent = categoriesdf.groupby("Descrizione operazioni").sum()
+
+        df_melted_catent = pd.melt(
+            df_sum_catent.reset_index(),
+            id_vars="Descrizione operazioni",
+            value_vars=df_sum_catent.reset_index().columns[1:],
+            var_name="Column",
+            value_name="Sum",
+        )
+
+        opcolor = st.checkbox("Mostra singole operazioni", key="opcolor")
+        if opcolor:
+            barplotent = px.bar(
+                df_melted_catent, x="Column", y="Sum", color="Descrizione operazioni"
+            )
+        else:
+            barplotent = px.bar(df_melted_catent, x="Column", y="Sum")
+        st.plotly_chart(barplotent)
+
+        st.write("### Analisi delle entrate per centri di spesa")
+        st.write(categoriesdf.describe())
+
+        # ---------------------------------------------------------------------------- #
+        #                                   Uscite                                    #
+        # ---------------------------------------------------------------------------- #
+
+        st.write("## Uscite per centri di spesa")
+        categoriesdf = dataframe.loc[:, "Cancelleria":"Altro.1"]
+        categoriesdf["Descrizione operazioni"] = dataframe["Descrizione operazioni"]
+
+        df_sum_catent = categoriesdf.groupby("Descrizione operazioni").sum()
+
+        df_melted_catent = pd.melt(
+            df_sum_catent.reset_index(),
+            id_vars="Descrizione operazioni",
+            value_vars=df_sum_catent.reset_index().columns[1:],
+            var_name="Column",
+            value_name="Sum",
+        )
+
+        opcolor = st.checkbox("Mostra singole operazioni", key="opcoloruscite")
+        if opcolor:
+            barplotent = px.bar(
+                df_melted_catent, x="Column", y="Sum", color="Descrizione operazioni"
+            )
+        else:
+            barplotent = px.bar(df_melted_catent, x="Column", y="Sum")
+        st.plotly_chart(barplotent)
+
+        st.write("### Analisi delle uscite per centri di spesa")
+        st.write(categoriesdf.describe())
+
+with tabadd:
+    # ----------------------------------- intro ---------------------------------- #
+    st.write("## Bozza form per inserimento spesa")
+    # st.write("valori positivi inseriscono entrate, valori negativi inseriscono uscite")
+
+    # --------------------------------- dataframe -------------------------------- #
+    try:
+        dfadd = pd.read_feather("dfadd.feather")
+    except Exception as e:
+        dfadd = pd.DataFrame(
+            columns=[
+                "Descrizione operazione",
+                "Data operazione",
+                "Conto",
+                "Categoria",
+                "Spesa",
             ]
+        )
+    # --------------------------------- funzioni --------------------------------- #
+    containervalue = st.container()
+    opzionientrata = [
+                        "Quote associative",
+                        "Erogazioni liberari",
+                        "Webinar e corsi",
+                        "Contributi pubblici",
+                        "Interessi attivi",
+                        "Attività economica",
+                        "Cessione a terzi beni di modesto valore/oggettistica",
+                        "Partecipazione Workshop/bioblitz",
+                    ]
+    opzioniuscita = [
+                        "Cancelleria",
+                        "Attrezzatura e spese tipografiche",
+                        "Postali e bollati",
+                        "Imposte e tasse",
+                        "Spese bancarie",
+                        "Costi informatici",
+                        "Costi Trasferte Missioni (viaggi+pasti)",
+                        "Compensi professionali",
+                        "Altro",
+                    ]
 
-    df_sum = (
-        dataframe.loc[:,analysecolumns+["Descrizione operazioni"]]
-        .groupby("Descrizione operazioni")
-        .sum()
-    )
-    
-    df_melted = pd.melt(df_sum.reset_index(), id_vars='Descrizione operazioni', value_vars=df_sum.reset_index().columns[1:], var_name='Column', value_name='Sum')
+    # ----------------------------------- form ----------------------------------- #
+    descrizione = st.text_input("Descrizione operazione")
+    data = st.date_input("Data operazione")
+    conto = st.selectbox("Conto", options=["Cassa", "Banca Prossima", "Paypal"])
+    with containervalue:
+        valore = st.number_input(
+            "Valore", key="valore"
+        )
+        categoria = st.selectbox(
+                    "Categoria",
+                    options=opzionientrata+opzioniuscita,
+                    key="categoria",
+        )
+    submit = st.button("Submit")
 
-    st.write("### Ammontare per operazione")
-
-    barplotadjacent = px.bar(df_melted, x='Descrizione operazioni', y='Sum', color='Column')
-    st.plotly_chart(barplotadjacent)
-
-    st.write("### Entrate e uscite")
-    barplotstack = px.bar(df_melted, x='Column', y='Sum', color='Descrizione operazioni', barmode="stack")
-    st.plotly_chart(barplotstack)
-
-
-    st.write("### Ammontare per singole operazioni")
-    st.write(df_sum.transpose())
+    # ---------------------------------- valori ---------------------------------- #
+    if submit:
+        row = pd.DataFrame(
+            data={
+                "Descrizione operazione" : descrizione,
+                "Data operazione" : data,
+                "Conto" : conto,
+                "Categoria" : categoria,
+                "Spesa" : valore,
+            },
+            index=[0]
+        )
+        dfadd = pd.concat([dfadd, row], ignore_index=True,)
+        dfadd.to_feather("dfadd.feather")
+    st.write(dfadd)
     
-    
-    # ---------------------------------------------------------------------------- #
-    #                                   Entrate                                    #
-    # ---------------------------------------------------------------------------- #
-
-    
-    st.write("## Entrate per centri di spesa")
-    categoriesdf = dataframe.loc[:,"Quote associative":"Altro"]
-    categoriesdf["Descrizione operazioni"] = dataframe["Descrizione operazioni"]
-    
-
-    
-    df_sum_catent = (
-        categoriesdf
-        .groupby("Descrizione operazioni")
-        .sum()
-    )
-    
-    df_melted_catent = pd.melt(df_sum_catent.reset_index(), id_vars='Descrizione operazioni', value_vars=df_sum_catent.reset_index().columns[1:], var_name='Column', value_name='Sum')
-
-    opcolor = st.checkbox("Mostra singole operazioni", key="opcolor")
-    if opcolor:
-        barplotent = px.bar(df_melted_catent, x='Column', y='Sum', color='Descrizione operazioni')
-    else:
-        barplotent = px.bar(df_melted_catent, x='Column', y='Sum')
-    st.plotly_chart(barplotent)
-    
-    st.write("### Analisi delle entrate per centri di spesa")
-    st.write(categoriesdf.describe())
-    
-    # ---------------------------------------------------------------------------- #
-    #                                   Uscite                                    #
-    # ---------------------------------------------------------------------------- #
-
-    
-    st.write("## Uscite per centri di spesa")
-    categoriesdf = dataframe.loc[:,"Cancelleria":"Altro.1"]
-    categoriesdf["Descrizione operazioni"] = dataframe["Descrizione operazioni"]
-    
-
-    
-    df_sum_catent = (
-        categoriesdf
-        .groupby("Descrizione operazioni")
-        .sum()
-    )
-    
-    df_melted_catent = pd.melt(df_sum_catent.reset_index(), id_vars='Descrizione operazioni', value_vars=df_sum_catent.reset_index().columns[1:], var_name='Column', value_name='Sum')
-
-    opcolor = st.checkbox("Mostra singole operazioni", key="opcoloruscite")
-    if opcolor:
-        barplotent = px.bar(df_melted_catent, x='Column', y='Sum', color='Descrizione operazioni')
-    else:
-        barplotent = px.bar(df_melted_catent, x='Column', y='Sum')
-    st.plotly_chart(barplotent)
-    
-    st.write("### Analisi delle uscite per centri di spesa")
-    st.write(categoriesdf.describe())
-    
-    
-    
-
-    
+    st.bar_chart(dfadd, x= "Categoria", y="Spesa")
